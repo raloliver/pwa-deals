075e229b25be94b62e0a82a3e56a966a1ad99c8a
Ral Oliver <raloliver.ws@gmail.com>
2017-04-23 01:20:06 -0300

    Connection Status

--- js/dealService.js
+++ js/dealService.js
@@ -4,27 +4,41 @@ define(['./template.js', './clientStorage.js'], function (template, clientStorag
     //var apiUrlFile = '../data.json';
     var apiUrlDeal = apiUrlPath + 'car.php?carId=';
 
-    function seeMoreDeals() {
-        fetch(apiUrlFile + "?carId=" + clientStorage.getLastDealId())
-            .then(function (response) {
-                return response.json();
-            }).then(function (data) {
-                //se alterar o serviço, por favor, altere aqui o valor ;)
-                clientStorage.addDeals(data.cars)
-                    .then(function () {
-                        template.appendDeals(data.cars);
-                        //loadMore();
-                    });
+    function seeMoreDeals() {        
+        fetchPromise()        
+            .then(function (status) {
+                document.querySelector("#connection-status").innerHTML = status; 
+                //loadMore();
             })
+    }
 
+    function fetchPromise() {
+        return new Promise(function (resolve, reject) {
+            fetch(apiUrlFile + "?carId=" + clientStorage.getLastDealId())
+                .then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                    //se alterar o serviço, por favor, altere aqui o valor
+                    clientStorage.addDeals(data.cars)
+                        .then(function () {
+                            template.appendDeals(data.cars);
+                            resolve('Internet funcionando.');
+                        });
+                }).catch(function () {
+                    resolve('Falha a conexão. Por favor, verifique a sua internet.')
+                });
+            setTimeout(function () {
+                resolve('Internet interrompida. Trabalhando offline...');
+            }, 3000)
+        })
     }
 
     function loadDealPage(dealId) {
         fetch(apiUrlDeal + dealId)
             .then(function (response) {
-                return response.text();                
+                return response.text();
             }).then(function (data) {
-                document.body.insertAdjacentHTML('beforeend', data);                           
+                document.body.insertAdjacentHTML('beforeend', data);
             }).catch(function (data) {
                 console.log(data);
                 console.error('Não foi possível encontrar essa oferta.');
